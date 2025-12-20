18022d19d3fdfd363ea13cdd1a8264b4c876b2ca
Garanich Denis <garanichofficial@gmail.com>
2025-12-20 07:05:11 +0300

    forgot transactions.py

--- application/schemas/transactions.py
+++ application/schemas/transactions.py
@@ -2,9 +2,8 @@ from pydantic import BaseModel, Field
 
 from datetime import datetime
 
-
 class TransactionsSchema(BaseModel):
-    amount: float
-    category: str = Field(max_length=32)
-    date: datetime
-    description: str | None = Field(max_length=512)
+    amount: float = Field(gt=0, description="The transaction amount must be greater than 0")
+    category: str = Field(max_length=32, description="Transaction category")
+    date: datetime | None = Field(None, description="Transaction datetime (default is current)")
+    description: str | None = Field(max_length=512, description="Transaction description")
